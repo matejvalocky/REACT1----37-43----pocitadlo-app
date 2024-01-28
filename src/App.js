270c2ea8911eb9a8d36e71ea39db8b759fbe53cc
Matej Valocký <matej.valocky@gmail.com>
2024-01-28 01:07:40 +0100

    40. React - Propojení useState, props a kliknutí na tlačítko

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { useState } from "react"
 const App = () => {
   const [value, setValue] = useState(0)
   
-  
+  const decreaseOne = () => {
+    const newValue = value - 1
+    setValue(newValue)
+  }
     
 
   return <div>
     <h1>Počítadlo</h1>
     <h2>{value}</h2>
-    <DecreaseButton />
+    <DecreaseButton decrease={decreaseOne} />
   </div>
 
 
--- src/components/DecreaseButton.js
+++ src/components/DecreaseButton.js
@@ -1,8 +1,8 @@
 import "./DecreaseButton.css"
 
 
-const DecreaseButton = () => {
-  return <button>
+const DecreaseButton = (props) => {
+  return <button onClick={props.decrease}>
     Znížiť o 1
   </button>
 }
